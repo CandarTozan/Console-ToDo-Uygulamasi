c91daa5553e350c4a7b6e04341e766d0accb540e
Candar TOZAN <candar.tozan@outlook.com>
2022-12-13 21:29:47 +0300

    Add project files.

---
+++ ToDo/Enum/EnumStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo.Enum
+{
+    public enum EnumStatus
+    {
+        TODO = 1,
+        INPROGRESS = 2,
+        DONE = 3
+    }
+}
